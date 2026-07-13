fab5d22df7b36f1abb6e00d56971192c7c6258fe
krish agrawal <krishagrawal706krish07@gmail.com>
2026-07-13 16:43:19 +0530

    Time: 7 ms (99.03%), Space: 49.3 MB (27.04%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length<=1){
+            return intervals;
+        }
+        // Sort intervals based on starting point
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> result = new ArrayList<>();
+
+        int[] newinterval = intervals[0];
+        result.add(newinterval);
+
+        for (int[] interval: intervals) {
+
+    
+            if (interval[0] <= newinterval[1]) {
+                newinterval[1] =Math.max(newinterval[1],interval[1]);
+            } 
+
+            else{
+                newinterval = interval;
+                result.add(newinterval);
+            }
+        }
+           
+        
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+
+}
